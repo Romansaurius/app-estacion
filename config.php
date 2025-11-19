54870389d708738acc628fc7ae09821f1cc9f255
Romansaurius <ranuccimanro07@gmail.com>
2025-11-19 02:22:31 -0300

    Simplificar envío de emails - Usar solo función mail() de PHP - Evitar errores de sintaxis con use

--- config.php
+++ config.php
@@ -38,42 +38,7 @@ function getClientIP() {
 }
 
 function sendEmail($to, $subject, $body) {
-    // Intentar usar PHPMailer si está disponible
-    if (file_exists('../PHPMailer/src/PHPMailer.php')) {
-        require_once '../PHPMailer/src/Exception.php';
-        require_once '../PHPMailer/src/PHPMailer.php';
-        require_once '../PHPMailer/src/SMTP.php';
-        
-        use PHPMailer\PHPMailer\PHPMailer;
-        use PHPMailer\PHPMailer\SMTP;
-        use PHPMailer\PHPMailer\Exception;
-        
-        $mail = new PHPMailer(true);
-        
-        try {
-            $mail->isSMTP();
-            $mail->Host       = 'mattprofe.com.ar';
-            $mail->SMTPAuth   = true;
-            $mail->Username   = '9909@mattprofe.com.ar';
-            $mail->Password   = 'buey.sauce.silla';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $mail->Port       = 587;
-            
-            $mail->setFrom('9909@mattprofe.com.ar', 'App Estación');
-            $mail->addAddress($to);
-            
-            $mail->isHTML(true);
-            $mail->Subject = $subject;
-            $mail->Body    = $body;
-            
-            $mail->send();
-            return true;
-        } catch (Exception $e) {
-            error_log("Error PHPMailer: {$mail->ErrorInfo}");
-        }
-    }
-    
-    // Fallback: usar función mail() básica
+    // Usar función mail() básica de PHP
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     $headers .= 'From: App Estación <9909@mattprofe.com.ar>' . "\r\n";
